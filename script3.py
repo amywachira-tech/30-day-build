e3d08fd3dda4f90fd191a71f20df0038b43f9acc
Amy Wachira <amywachira@gmail.com>
2026-08-19 09:58:17 +0300

    script 3: dictionary basics

---
+++ script3.py
@@ -0,0 +1,9 @@
+prospect = {
+    "name": "Jordan Kim",
+    "company": "Nova Energy",
+    "email": "jordan.kim@novaenergy.com"
+}
+
+print(prospect["name"])
+print(prospect["company"])
+print(prospect["email"])

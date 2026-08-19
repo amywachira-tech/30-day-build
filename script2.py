9f9ef96f76aa75ce1aeee84bcd95d6e2e4a438d6
Amy Wachira <amywachira@gmail.com>
2026-08-19 09:54:33 +0300

    script 2: loop over a list

---
+++ script2.py
@@ -0,0 +1,5 @@
+prospects = ["Acme Corp", "Nova Energy", "Bright Path", "Kestrel Labs", "Delta Freight",
+             "Solaris Group", "Northwind Trading", "Vertex Systems", "Rivera & Co", "Atlas Media"]
+
+for prospect in prospects:
+    print(prospect)
